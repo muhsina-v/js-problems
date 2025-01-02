92952b085372efb092e730c8fce14f4c589b5434
muhsina <muhsinamolty44@gmail.com>
2025-01-02 15:04:51 +0530

    brackets.js

---
+++ bracets.js
@@ -0,0 +1,17 @@
+function isValid(s) {
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } else {
+            let last = stack.pop();
+            if ((char === ')' && last !== '(') || (char === '}' && last !== '{') || (char === ']' && last !== '[')) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+}
+
+console.log(isValid("()"));
