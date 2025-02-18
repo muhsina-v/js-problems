13a4cf96695a1e16b85e188a2308458410cd0fb1
muhsina <muhsinamolty44@gmail.com>
2025-02-18 16:52:26 +0530

    mySqrt

---
+++ mySqrt.js
@@ -0,0 +1,21 @@
+function mySqrt(x) {
+    if (x ===0 || x ===1) return x;
+
+    let left =0,right=x,result = 0;
+
+    while (left <=right) {
+        let mid = Math.floor((left+right) /2);
+
+        if (mid *mid===x) {
+            return mid;
+        } else if (mid *mid< x) {
+            result = mid; 
+            left = mid+1;
+        } else {
+            right=mid-1;
+        }
+    }
+    return result;
+}
+
+console.log(mySqrt(4)); 
