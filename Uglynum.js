47e0e48cd400c1bcbd03b93f8a6307d275bc7dc8
muhsina <muhsinamolty44@gmail.com>
2025-03-11 16:26:08 +0530

    isUgly

---
+++ Uglynum.js
@@ -0,0 +1,12 @@
+function isUgly(n) {
+    if (n<=0) return false;
+    if (n===1) return true;
+    if (n%2===0) return isUgly(n/2);
+    if (n%3===0) return isUgly(n/3);
+    if (n%5===0) return isUgly(n/5);
+    return false
+}
+
+console.log(isUgly(6));   
+console.log(isUgly(1));   
+console.log(isUgly(14));  
