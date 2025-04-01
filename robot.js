6041ee6119522c5c33dc91295aa5d064be6c6bf0
muhsina <muhsinamolty44@gmail.com>
2025-04-01 09:16:57 +0530

    judgeCircle/robot

---
+++ robot.js
@@ -0,0 +1,16 @@
+var robot=function(moves){
+    let x=0,y=0;
+
+    for(let move of moves){
+        if(move=='R')x++;
+        else if(move=='L') x--;
+        else if(move=='U') y++;
+        else if(move=='D') y--;
+
+    }
+
+    return x==0 && y==0;
+    
+}
+console.log(robot("LR"));
+
